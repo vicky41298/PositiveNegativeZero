a152d923e549650087fa01848499ff549c999a81
vicky41298 <vicky41298@gmail.com>
2019-07-02 15:40:41 +0530

    Create VC.py

---
+++ VC.py
@@ -0,0 +1,14 @@
+a=input()
+if a == "A" or a == "a":
+	print("Vowel")
+elif a == "E" or a == "e":
+	print("Vowel")
+elif a == "I" or a == "i":
+	print("Vowel")
+elif a == "O" or a == "o":
+	print("Vowel")
+elif a == "U" or a == "u":
+	print("Vowel")
+else:
+	print("Consonant")
+
