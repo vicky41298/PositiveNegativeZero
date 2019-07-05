5aea30407b5228a4e86d3d2c9caeb9be5564c1f2
vicky41298 <vicky41298@gmail.com>
2019-07-05 22:41:04 +0530

    Update dectp.py

--- dectp.py
+++ dectp.py
@@ -1,8 +1,8 @@
 te = int(input())
 lis1 = list(map(int,input().split()))
 su = 0
-for i in range(1,len(lis1)):
-    for j in range(len(lis1)):
-        if lis1[j]<lis1[i]:
-            su+=lis1[j]
+for op in range(1,len(lis1)):
+    for vi in range(op):
+        if lis1[vi]<lis1[op]:
+            su+=lis1[vi]
 print(su)
