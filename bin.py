44ad1c0c586240331f57aa0c4a0243bcc890db7a
vicky41298 <vicky41298@gmail.com>
2019-07-05 11:48:05 +0530

    Update bin.py

--- bin.py
+++ bin.py
@@ -1,4 +1,13 @@
-arr=int(input())
-brr=list(map(int,input().split()))[:arr]
-brr=sorted(brr,reverse=True)
-print(*brr,sep="\n")
+te = int(input())
+birr = list(map(int,input().split()))
+cop = []
+for i in birr:
+  cop1 = bin(i)
+  cop.append(cop1)
+ne = sorted(cop,reverse=True)
+#print(e5)
+for k in ne:
+  print(int(k,2))
+#qw = list(map(str,input().split()))
+#qw5 = sorted(qw,reverse=True)
+#print(qw5)
