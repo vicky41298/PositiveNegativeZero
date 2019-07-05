1ec47ea5ac137805bb6172940c41ce3753500aec
vicky41298 <vicky41298@gmail.com>
2019-07-05 19:19:34 +0530

    Update sumsandno.py

--- sumsandno.py
+++ sumsandno.py
@@ -1,14 +1,13 @@
 x,j=map(int,input().split())
 y=list(map(int,input().split()))[:x]
+count=0
 for i in range(0,len(y)-1):
   for sec in range(i+1,len(y)-1):
-    if(y[i]==y[sec]):
-      su=y[sec]
-#print(su) 
-su=su+su
-#print(j)
-#print(su+su)   
-if(su==j):
+    if(y[i]+y[sec]==j):
+      count+=1  
+#print(count) 
+#print(j)   
+if count==1:
   print("yes")
 else:
   print("no")
