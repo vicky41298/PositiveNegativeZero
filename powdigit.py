81cecbf79af9468ebe905e89be42d76a94d270ee
vicky41298 <vicky41298@gmail.com>
2019-07-08 20:41:45 +0530

    Update powdigit.py

--- powdigit.py
+++ powdigit.py
@@ -1,6 +1,10 @@
 numpp=input()
 sumo=0
 convo=int(numpp)
-for i in range(len(numpp)):
-  sumo+=(int(numpp[i])**int(numpp[-1]))
-print(sumo)
+if len(numpp)==1:
+  print(numpp)
+else:
+  for i in range(len(numpp)):
+    sumo+=(int(numpp[i])**int(numpp[-1]))
+
+  print(sumo)
