d06ca75b78267a912632ea3ab19392ba367a4d6b
vicky41298 <vicky41298@gmail.com>
2019-07-11 22:44:47 +0530

    Update mincostofstri.py

--- mincostofstri.py
+++ mincostofstri.py
@@ -1,7 +1,7 @@
 te1,te2=input().split()
-tem=abs(len(te1)-len(te2))
+tem=abs(len(te2)-len(te1))
 for i in range(len(te1)):
-  if(len(te2)==1 and te2[i] not in te1):
+  if(len(te2)==1 and te2[i] in te1):
     break
   if(te1[i]!=te2[i]):
     tem=tem+1
